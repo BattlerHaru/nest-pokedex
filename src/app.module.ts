8f5714f428b91a6285f23da2b57cba023baaface
BattlerHaru <kronk.sdance@gmail.com>
2024-04-30 21:56:37 -0600

    :wrench: add .env and read .env on app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,20 @@
-import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { PokemonModule } from './pokemon/pokemon.module';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
 
 
     PokemonModule,
@@ -24,4 +26,8 @@ import { SeedModule } from './seed/seed.module';
     SeedModule
   ],
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+
+  }
+}
